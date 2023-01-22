6405e0717a0ce6d88ac31541e8ca3baf078c2063
TeksuSiK <oskar@teksusik.pl>
2023-01-22 14:35:10 +0100

    Add plugin.yml and main plugin class

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,6 +20,12 @@ dependencies {
     compileOnly("com.destroystokyo.paper:paper-api:1.13.2-R0.1-SNAPSHOT")
 }
 
+tasks.processResources {
+    expand(
+            "experienceTomeVersion" to version,
+    )
+}
+
 publishing {
     repositories {
         maven {
---
+++ src/main/java/pl/teksusik/experiencetome/ExperienceTomePlugin.java
@@ -0,0 +1,14 @@
+package pl.teksusik.experiencetome;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ExperienceTomePlugin extends JavaPlugin {
+    @Override
+    public void onEnable() {
+    }
+
+    @Override
+    public void onDisable() {
+    }
+}
+
