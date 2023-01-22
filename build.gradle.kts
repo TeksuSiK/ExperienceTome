e442ce64de47181d658ccf97e9866ada9b700506
TeksuSiK <oskar@teksusik.pl>
2023-01-22 14:32:19 +0100

    Change version to 0.1-SNAPSHOT, add required plugins and repository publishing, add paper-api dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,19 +1,52 @@
 plugins {
     id("java")
+    id("java-library")
+    id("maven-publish")
 }
 
 group = "pl.teksusik"
-version = "1.0-SNAPSHOT"
+version = "0.1-SNAPSHOT"
+
+apply(plugin = "java")
+apply(plugin = "java-library")
+apply(plugin = "maven-publish")
 
 repositories {
     mavenCentral()
+    maven(url = "https://papermc.io/repo/repository/maven-public/")
 }
 
 dependencies {
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+    compileOnly("com.destroystokyo.paper:paper-api:1.13.2-R0.1-SNAPSHOT")
 }
 
-tasks.getByName<Test>("test") {
-    useJUnitPlatform()
+publishing {
+    repositories {
+        maven {
+            name = "teksusik"
+            url = uri("https://repo.teksusik.pl/${if (version.toString().endsWith("-SNAPSHOT")) "snapshots" else "releases"}")
+            credentials {
+                username = System.getenv("MAVEN_NAME")
+                password = System.getenv("MAVEN_TOKEN")
+            }
+        }
+    }
+
+    publications {
+        create<MavenPublication>("library") {
+            from(components.getByName("java"))
+
+            // Add external repositories to published artifacts
+            pom.withXml {
+                val repositories = asNode().appendNode("repositories")
+                project.repositories.findAll(closureOf<Any> {
+                    if (this is MavenArtifactRepository && this.url.toString().startsWith("https")) {
+                        val repository = repositories.appendNode("repository")
+                        repository.appendNode("id", this.url.toString().replace("https://", "").replace("/", "-").replace(".", "-").trim())
+                        repository.appendNode("url", this.url.toString().trim())
+                    }
+                })
+            }
+        }
+    }
 }
